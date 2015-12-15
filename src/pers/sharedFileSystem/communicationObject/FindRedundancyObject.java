db85b652772990dd8bb4b1aba0118f6dc5dcf2ab
buaashuai <shuaiwang126@163.com>
2015-12-15 14:51:59 +0800

    实现序列号接口

--- src/pers/sharedFileSystem/communicationObject/FindRedundancyObject.java
+++ src/pers/sharedFileSystem/communicationObject/FindRedundancyObject.java
@@ -1,9 +1,11 @@
 package pers.sharedFileSystem.communicationObject;
 
+import java.io.Serializable;
+
 /**
  * 查找冗余信息文件通信对象
  */
-public class FindRedundancyObject {
+public class FindRedundancyObject implements Serializable {
     /**
      * 文件指纹信息
      */
